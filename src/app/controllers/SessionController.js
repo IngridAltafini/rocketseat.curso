1f4af51a6b40cf502bed96cfcad5e098d6d069d4
Ingrid Altafini <ingridalatfini225@gmail.com>
2022-07-07 17:34:23 -0300

    validando dados de entrada

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,24 +1,36 @@
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'
 
-import authConfig from '../../config/auth';
+import * as Yup from 'yup'
 
-import User from '../models/User';
+import User from '../models/User'
+import authConfig from '../../config/auth'
 
 class SessionController {
   async store(request, response) {
-    const { email, password } = request.body;
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    })
 
-    const user = await User.findOne({ where: { email } });
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails'})
+    }
+
+    const { email, password } = request.body
+
+    const user = await User.findOne({ where: { email }})
 
     if (!user) {
-      return response.status(401).json({ error: 'User not found' });
+      return response.status(401).json({ error: 'user not found'})
     }
 
-    if (!(await user.checkPassword(password))) {
-      return response.status(401).json({ error: 'Password does not match' });
+    if (!await user.checkPassword(password)) {
+      return response.status(401).json({ error: 'password does not match'})
     }
 
-    const { id, name } = user;
+    const { id, name } = user
 
     return response.json({
       user: {
@@ -27,10 +39,11 @@ class SessionController {
         email,
       },
       token: jwt.sign({ id }, authConfig.secret, {
-        expiresIn: authConfig.expiresIn,
-      }),
-    });
+        expiresIn: authConfig.expiresIn
+      })
+    })
   }
 }
 
-export default new SessionController();
+
+export default new SessionController()
